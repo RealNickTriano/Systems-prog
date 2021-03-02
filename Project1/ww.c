c23e79d712f1f1ebf1396ca0ce83d192055dae68
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-03-02 17:20:50 -0500

    reads file then prints to stdout with width variable (getting stack smashing error)

--- Project1/ww.c
+++ Project1/ww.c
@@ -8,6 +8,12 @@
 
 int main(int argc, char **argv)
 {
+    strbuf_t buf;
+    sb_init(&buf, 100);
+    int bytes, i;
+    int width = 20;
+    int BUFSIZE = 256;
+    //char buf[BUFSIZE];
     if(argc > 2 || argc == 0)               // Make sure enough arguements are passed
     {
         return EXIT_FAILURE;
@@ -19,6 +25,17 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
+    while(( bytes = read(fd, &buf, BUFSIZE)) > 0 )       // while we are reading 
+    {
+        write(0, &buf, (sizeof(char) * width));
+	    putchar('\n');
+	
+    }
+
+    if (bytes < 0)                             // if bytes read are negative??
+    {
+        perror("Read error");
+    }
 
     close(fd);                               // closes file
 }
