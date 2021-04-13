f70307ea82713172fb15646b27195fc9cda11c06
Shihab00 <66040531+Shihab00@users.noreply.github.com>
2021-04-12 20:28:00 -0400

    A wild idea

---
+++ Project2/FC3.c
@@ -0,0 +1,267 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <signal.h>
+#include <pthread.h>
+#include "strbuf.h"
+#include "fileQueue.h"
+#include "linkedlist.h"
+#include "wfdlinkedlist.h"
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#ifndef BUFSIZE
+#define BUFSIZE 128
+#endif
+
+int directory_threads = 1, file_threads = 1, analysis_threads = 1;
+char file_name_suffix[] = ".txt";
+int files = 0;
+node_wfd_t *wfd_repo;
+
+int is_directory(const char *path) // !=0 if file is directory
+{
+    struct stat statbuf;
+    stat(path, &statbuf);
+    return S_ISDIR(statbuf.st_mode);
+}
+
+int is_file(const char *path) //!=0 if file is regular file
+{
+    struct stat statbuf;
+    stat(path, &statbuf);
+    return S_ISREG(statbuf.st_mode);
+}
+
+int FindWFD(char *path)
+{
+    int bytes_read, input_fd;
+    float words_found = 0;
+    float wfd = 0.0;
+    char buf[BUFSIZE];
+    char character; 
+    //char adjacent_character;
+    int found_end = 0; //A boolean to check if we found the end of a word
+    int word_len = 0;
+    int word_start = 0; //The start position of a word
+    //int word_end = 0;   //The end position of a word
+    char *word;
+    node_t *list;
+    strbuf_t *str;
+    sb_init(word, BUFSIZE);
+
+    files++;
+    input_fd = open(path, O_RDONLY);
+    while ((bytes_read = read(input_fd, buf, BUFSIZE)) > 0) //reading file
+    {
+        // convert letters to all lowercase
+        for (int i = 0; i < bytes_read; i++)
+        {
+            buf[i] = tolower(buf[i]);
+        }
+
+        for (int i = 0; i < bytes_read; i++)
+        {
+            character = buf[i];
+
+            if (isalpha(character) != 0 || isdigit(character) != 0 || character == '-')
+            {
+                word_start = 1;
+                sb_append(str, character);
+                word_len++;
+            }
+            else if (isspace(character) != 0)
+            {
+                if (word_start == 1)
+                {
+                    found_end = 1;
+                    word_start = 0;
+                }
+                words_found++;
+            }
+
+            if (found_end == 1)
+            {
+                found_end = 0; // reset found_end
+
+                word = malloc(sizeof(char) * word_len);
+                strncpy(word, &sb_word(str, word_len), word_len);
+
+                sb_destroy(str);
+                sb_init(str, BUFSIZE);
+
+                /* if(DEBUG)
+                {
+                    printf("%s\n", word);
+                }*/
+                // now we can add the word to linked list
+                if (words_found == 1)
+                {
+                    list = initNode(word);
+                }
+                else
+                {
+                    list = add(list, word);
+                }
+            }
+        }
+    }
+    // done reading file
+    if (DEBUG)
+    {
+        printList(list);
+    }
+
+    // now calculate WFD of file
+    while (list != NULL)
+    {
+        wfd = (list->count / words_found);
+        if (DEBUG)
+        {
+            printf("%.2lf ", wfd);
+        }
+
+        list->frequency = wfd;
+        list = list->next;
+    }
+
+    // add to WFD repo
+   /* if(files == 1) // if its the first file
+    {
+        wfd_repo = initNode2(path);
+    }
+    else 
+    {
+    wfd_repo = add2(wfd_repo, path, list);
+    }*/
+}
+int SetOptions(char *argv) // sets number of threads/ file name suffix
+{
+    if (strncmp(argv, "-d", sizeof(char) * 2) == 0)
+    {
+        if (atoi(&argv[2]) <= 0)
+        {
+            fprintf(stderr, "Invalid number of threads: %s\n", argv);
+            exit(EXIT_FAILURE);
+        }
+        else
+            directory_threads = atoi(&argv[2]);
+    }
+    else if (strncmp(argv, "-f", sizeof(char) * 2) == 0)
+    {
+        if (atoi(&argv[2]) <= 0)
+        {
+            fprintf(stderr, "Invalid number of threads: %s\n", argv);
+            exit(EXIT_FAILURE);
+        }
+        else
+            file_threads = atoi(&argv[2]);
+    }
+    else if (strncmp(argv, "-a", sizeof(char) * 2) == 0)
+    {
+        if (atoi(&argv[2]) <= 0)
+        {
+            fprintf(stderr, "Invalid number of threads: %s\n", argv);
+            exit(EXIT_FAILURE);
+        }
+        else
+            analysis_threads = atoi(&argv[2]);
+    }
+    else if (strncmp(argv, "-s", sizeof(char) * 2) == 0)
+    {
+        // set new file name suffix
+        int len = strlen(argv) - 2;
+        strncpy(file_name_suffix, argv + 2, len);
+    }
+    else
+    {
+        fprintf(stderr, "Invalid optional argument: %s\n", argv);
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
+
+int CheckArgs(char **argv, int argc, int opt_arg_count) // Checks arguments seperates optional/regular
+{
+    // number of optional arguments inputed
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strncmp(argv[i], "-", sizeof(char)) == 0) // found an optional arg
+        {
+            SetOptions(argv[i]); // set thread/file suffix value
+            opt_arg_count++;
+        }
+    }
+
+    return opt_arg_count;
+}
+int main(int argc, char **argv)
+{
+    int opt_arg_count = 0;
+    struct targs *args;
+    pthread_t *tids;
+
+    if (argc < 3) //not enough arguments
+    {
+        return EXIT_FAILURE;
+    }
+
+    queue_t file_q; // Create and initialize file/directory queues
+    init(&file_q);
+    queue_t directory_q;
+    init(&directory_q);
+
+    opt_arg_count = CheckArgs(argv, argc, opt_arg_count); // Check for optional arguments
+
+    // start threads here:
+    for (int i = 1; i < argc; i++)
+    {
+        if (is_directory(argv[i]) != 0) // found a directory
+        {
+            // add to directory queue
+            enqueue(&directory_q, argv[i]);
+        }
+        else if (is_file(argv[i]) != 0) // found a file
+        {
+            // add to file queue
+            enqueue(&file_q, argv[i]);
+        }
+        else
+        {
+        }
+    }
+
+    while(file_q.count != 0)
+    {
+        char *path;
+        path = dequeue(&file_q, path);
+        if (path == NULL)
+        {
+            return 0;
+        }
+        FindWFD(path);
+    }
+    
+    if (DEBUG)
+    {
+        printf("File Queue...\n");
+        printQueue(&file_q);
+        printf("Directory Queue...\n");
+        printQueue(&directory_q);
+    }
+
+    if (DEBUG)
+    {
+        printf("dir threads: %d\nfile threads: %d\nanalysis threads: %d\noptional arguments: %d\n", directory_threads, file_threads, analysis_threads, opt_arg_count);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ Project2/strbuf.h
@@ -0,0 +1,141 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+typedef struct {
+    size_t length;
+    size_t used;
+    char *data;
+} strbuf_t;
+
+int sb_init(strbuf_t *S, size_t length)
+{
+        S->data = malloc(sizeof(char) * length);
+        if (!S->data) return 1;
+
+        S->length = length;
+        S->used   = 1;
+        S->data[0] = '\0';
+
+        return 0;
+}
+
+void sb_destroy(strbuf_t *S)
+{
+        free(S->data);
+}
+
+int sb_append(strbuf_t *S, char item)
+{
+        if (S->used >= S->length) {
+                size_t size = S->length * 2;
+                char *p = realloc(S->data, sizeof(char) * size);
+                if (!p) return 1;
+
+                S->data = p;
+                S->length = size;
+
+                if (DEBUG) printf("Increased size to %lu\n", size);
+        }
+
+        if (DEBUG) printf("Appeneding item %c \n", item);
+        S->data[S->used - 1] = item;
+        S->data[S->used] = '\0';
+        ++S->used;
+
+        return 0;
+}
+
+
+int sb_remove(strbuf_t *S, char *item)
+{
+        if (S->used == 0) return 1;
+
+        --S->used;
+
+        S->data[S->used] = S->data[S->used -1];
+        S->data[S->used -1] = '\0';
+
+        if (DEBUG) printf("Removed item %c \n", S->data[S->used]);
+
+        if (item) *item = S->data[S->used];
+
+        return 1;
+}
+
+int sb_insert(strbuf_t *S, int index, char item){
+        if (DEBUG) printf("Inserting item %c at index %d \n", item, index);
+        if (index + 1 >= S->length){
+                if (index + 1 >= (S->length * 2)){
+                        size_t size = index + 2;
+                        char *p = realloc(S->data, sizeof(char) * size);
+                        if (!p) return 1;
+                        
+                        S->data = p;
+                        S->length = size;
+
+                        if (DEBUG) printf("Increased size to %lu\n", size);
+                }
+                else{
+                        size_t size = S->length * 2;
+                        char *p = realloc(S->data, sizeof(char) * size);
+                        if (!p) return 1;
+
+                        S->data = p;
+                        S->length = size;
+
+                        if (DEBUG) printf("Increased size to %lu\n", size);
+                }
+
+                S->data[index] = item;
+                S->data[index + 1] = '\0';
+                S->used = index + 2;
+                return 0;
+        }
+        else if (index >= S->used){
+                S->data[index] = item;
+                S->data[index + 1] = '\0';
+                S->used = index + 2;
+
+                return 0;
+        }
+
+        //if (index < S->used){
+        else{
+                if (S->used == S->length - 1){
+                        size_t size = S->length * 2;
+                        char *p = realloc(S->data, sizeof(char) * size);
+                        if (!p) return 1;
+
+                        S->data = p;
+                        S->length = size;
+
+                        if (DEBUG) printf("Increased size to %lu\n", size);
+                }
+                else{
+                        for (int i = S->used; i > index; i--)
+                                S->data[i] = S->data[i - 1];
+                }
+                S->data[index] = item;
+                ++S->used;
+        }
+
+        return 0;
+}
+
+int sb_concat(strbuf_t *S, char *str){
+        for (int i = 0; i < strlen(str); i++){
+                sb_append(S, str[i]);
+        }
+        return 0;
+}
+
+const char* sb_word(strbuf_t *S, int len){
+        char str[len];
+        strncpy(str, S->data, len);
+        return str;
+}
